5882c197eeb0c1063fc30b726dde1d04b15d1c66
hamza.malik <hamza.malik@animapoint.net>
2025-12-01 11:45:10 +0800

    Purned

--- init.lua
+++ init.lua
@@ -2,6 +2,5 @@ require("config.keymaps")
 require("config.options")
 require("config.autocmds")
 require("core.lazy")
--- require("core.lsp")
 
 vim.g.python3_host_prog = vim.fn.expand("$HOME") .. "/.pyenv/shims/python"
